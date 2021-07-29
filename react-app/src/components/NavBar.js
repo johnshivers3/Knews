03be4ba31d2134470c61f6ba7eecb9bf04183291
John Shivers <johnshivers3@gmail.com>
2021-07-29 17:32:53 -0400

    add form and nav style

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,48 +1,52 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import LogoutButton from "./auth/LogoutButton";
-import { Arrows } from "./images/Arrows";
+import { Logo } from "./images/Logo";
 import "./NavBar.css";
 
 const NavBar = () => {
   return (
     <nav>
-      <div id="logo-nav">LOGO</div>
-      <div id="middle-nav">MIDDLE</div>
-      <input type="checkbox" id="hidden-check"></input>
-      <label htmlFor="hidden-check" className='transition-wrapper'>
-        <div className="transition-wrapper">
-          <ul>
-            <li>
-              <NavLink to="/" exact={true} activeClassName="active">
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/login" exact={true} activeClassName="active">
-                Login
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/sign-up" exact={true} activeClassName="active">
-                Sign Up
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/users" exact={true} activeClassName="active">
-                Users
-              </NavLink>
-            </li>
-            <li>
-              <LogoutButton />
-            </li>
-            <li></li>
-            <li>
-              <Arrows id='menu-arrows'/>
-            </li>
-          </ul>
-        </div>
-      </label>
+      <div id="logo-nav">
+        <Logo />
+      </div>
+      <div id="middle-nav"></div>
+      <div id="links-nav">
+        <input type="checkbox" id="hidden-check"></input>
+        <label htmlFor="hidden-check" className="transition-wrapper">
+          <div className="transition-wrapper">
+            <ul>
+              <li>
+                <NavLink to="/" exact={true} activeClassName="active">
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/login" exact={true} activeClassName="active">
+                  Login
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/sign-up" exact={true} activeClassName="active">
+                  Sign Up
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/users" exact={true} activeClassName="active">
+                  Users
+                </NavLink>
+              </li>
+              <li>
+                <LogoutButton />
+              </li>
+              <li></li>
+              <li id='ellipsis-li'>
+                <i className="fas fa-ellipsis-h"></i>
+              </li>
+            </ul>
+          </div>
+        </label>
+      </div>
     </nav>
   );
 };
--- react-app/src/components/images/Arrows.js
+++
@@ -1,5 +0,0 @@
-import arrows from '../../source-images/menu-arrows.png'
-
-export const Arrows = () => {
-  return <img src={arrows} alt='menu-arrows' height='50px' width='50px'/>
-}

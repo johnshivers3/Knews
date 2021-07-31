06118fea3899d561a252b490489b2af3cc9a830c
John Shivers <johnshivers3@gmail.com>
2021-07-31 19:53:19 -0400

    update

--- react-app/src/components/NewsFeed/NewsFeed.js
+++ react-app/src/components/NewsFeed/NewsFeed.js
@@ -1,19 +1,19 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { useHistory } from "react-router-dom";
+// import { useHistory } from "react-router-dom";
 import * as newsFeedActions from "../../store/newsfeed.js";
 import "./NewsFeed.css";
 
 export const NewsFeed = () => {
-  const [newsfeed, setNewsFeed] = useState();
-  const [errors, setErrors] = useState();
+  // const [newsfeed, setNewsFeed] = useState();
+  // const [errors, setErrors] = useState();
   const dispatch = useDispatch();
-  const history = useHistory();
+  // const history = useHistory();
   const topHeadlines = useSelector((state) => state.newsfeed.news?.articles);
 
   useEffect(() => {
     dispatch(newsFeedActions.getTopHeadlines());
-  }, []);
+  }, [dispatch]);
 
   return (
     <>
